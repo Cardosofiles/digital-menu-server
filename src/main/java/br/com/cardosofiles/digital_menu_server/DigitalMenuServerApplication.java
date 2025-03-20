e6f5bfb0a8695e651d2054a6277b3142feec4a92
cardosofiles <cardosofiles@outlook.com>
2025-03-19 22:18:13 -0300

    feat: starting application server

---
+++ src/main/java/br/com/cardosofiles/digital_menu_server/DigitalMenuServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.cardosofiles.digital_menu_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalMenuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalMenuServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cardosofiles/digital_menu_server/DigitalMenuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cardosofiles.digital_menu_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMenuServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
